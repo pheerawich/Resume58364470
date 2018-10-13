c87127ab2a7a3f36d99534176167622c2fde36c6
Pheerawich <35694790+pheerawich@users.noreply.github.com>
2018-10-13 08:09:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express()
 const port = 8000
 
 app.use(express.static(__dirname));
-app.get('/index.html',(req,res)=>res.sendFile(__dirname+"/"+"index.html"))
+app.get('/',(req,res)=>res.sendFile(__dirname+"/"+"index.html"))
 app.listen(port, () => console.log('app listening on port'+port))
